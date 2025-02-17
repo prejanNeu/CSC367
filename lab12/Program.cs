cedcb15ca02fcddb1e978ac3d0409d6448581a8f
prejan neupane <neupaneprejan2018@gmail.com>
2025-02-17 15:20:11 +0545

    -12

---
+++ lab12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+
+namespace Lab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("example.txt");
+            Console.WriteLine("Enter any text :");
+            string str = Console.ReadLine();
+
+            sw.WriteLine(str); //Write into the buffer
+            sw.Flush();
+            sw.Close();
+
+
+
+            StreamReader sr = new StreamReader("hello.txt");
+
+            sr.BaseStream.Seek(0,SeekOrigin.Begin); //move file pointer to the begining of the file 
+            string text = sr.ReadLine();
+
+            while (text!=null)
+            {
+                Console.WriteLine(text);
+                text=sr.ReadLine();
+            }
+
+            sr.Close();
+
+
+
+        }
+    }
+}
