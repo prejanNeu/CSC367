224517667e1612a438ae748ed8912995b33e22de
prejan neupane <neupaneprejan2018@gmail.com>
2025-02-17 22:39:31 +0545

    add all

---
+++ lab10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;//non generic collections 
+using System.Collections.Generic;
+
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList nongen = new ArrayList();
+            nongen.Add(10);
+            nongen.Add(20);
+            nongen.Add("Hello");
+            nongen.Add(3.5);
+
+            Console.WriteLine("<---Non generic collection (ArrayList)");
+            foreach (var item in nongen)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("<----Generic collection (List)------->");
+
+            List<int> list = new List<int>();
+            list.Add(20);
+            list.Add(11);
+            list.Add(0);
+
+            foreach (int num in list)
+            {
+                Console.WriteLine(num);
+            }
+
+            
+        }
+    }
+}
---
+++ lab11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab11
+{
+    public class Generic <T>
+    {
+        private T data;
+
+        public T value 
+        {
+            get 
+            {
+                return this.data;
+            }
+            set 
+            {
+                this.data= value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generic<int> obj1 = new Generic<int>();
+            obj1.value= 20;
+            Console.WriteLine(obj1.value);
+
+            Generic<string> obj2 = new Generic<string> ();
+            obj2.value= "hello";
+            Console.WriteLine(obj2.value);
+
+            Generic<double> obj3 = new Generic<double> ();
+            obj3.value= 5.999;
+            Console.WriteLine(obj3.value);
+
+
+        }
+    }
+}
